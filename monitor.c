31820919aed69e47f08a78bef8c450c824644328
Bruno Randolf <br1@einfach.org>
2016-07-26 13:04:23 +0100

    Use ESP8266 parsing function

--- monitor.c
+++ monitor.c
@@ -21,45 +21,7 @@ static unsigned int ntimeout;
 static struct uwifi_packet pkt;
 
 void ICACHE_FLASH_ATTR monitor_rx(uint8 *buf, uint16 len) {
-	struct sniffer_buf* sb;
-	struct sniffer_buf2* sb2;
-	struct RxControl* rxc = NULL;
-	uint8_t* frame = NULL;
-	int frame_len = 0;
-
-	if (len == 12) {
-		// unreliable, no MAC, no header length, no AMPDU
-		// has: rssi, FEC_CODING
-		rxc = (struct RxControl*)buf;
-		DBG_PRINT("--- RX ???? RSSI %d\n", rxc->rssi);
-		os_printf("~");
-	}
-	else if (len == 128) {
-		// MGMT: 112 bytes of header & data
-		sb2 = (struct sniffer_buf2*)buf;
-		DBG_PRINT("--- RX MGMT cnt %d len %d RSSI %d\n",
-			  sb2->cnt, sb2->len, sb2->rx_ctrl.rssi);
-		os_printf(",");
-		rxc = &sb2->rx_ctrl;
-		frame = sb2->buf;
-		frame_len = 112;
-	}
-	else {
-		// DATA: 36 bytes of header
-		sb = (struct sniffer_buf*)buf;
-		DBG_PRINT("--- RX DATA cnt %d len %d RSSI %d\n",
-			  sb->cnt, sb->lenseq[0].length, sb->rx_ctrl.rssi);
-		os_printf(".");
-		rxc = &sb->rx_ctrl;
-		frame = sb->buf;
-		frame_len = 36;
-	}
-
-	if (rxc != NULL && frame != NULL && frame_len != 0) {
-		os_memset(&pkt, 0, sizeof(pkt));
-		pkt.phy_signal = rxc->rssi;
-
-		uwifi_parse_80211_header((unsigned char*)frame, frame_len, &pkt);
+	if (uwifi_esp_parse(buf, len, &pkt)) {
 		uwifi_node_update(&pkt, &nodes);
 	}
 }
